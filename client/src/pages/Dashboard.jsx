6db5ae8c52ac90b2159c03c4bdf717a7b501c333
woody <gelius24@hotmail.com>
2024-04-07 10:57:06 -0400

    sidebar section of dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+import {useLocation} from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
+
+// this page have differents tabs (for admin)
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('')
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if (tabFromUrl) setTab(tabFromUrl)
+  }, [location.search])
+
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      {/* sidebar */}
+      <div className='md:w-56'>
+        <DashSidebar />
+      </div>
+      {/* right side */}
+      <div>
+        {tab === 'profile' && <DashProfile />}
+      </div>
+    </div>
   )
 }
