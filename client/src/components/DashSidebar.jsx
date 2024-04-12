e957bcdde3ef53be6ad20ecc03961ec538e12b6f
woody <gelius24@hotmail.com>
2024-04-12 16:45:32 -0400

    dashboard users page (admins)

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
 import {useDispatch, useSelector} from 'react-redux'
@@ -49,6 +49,13 @@ export default function DashSidebar() {
             currentUser.isAdmin && <Link to='/dashboard?tab=posts'>
             <Sidebar.Item active={tab === 'post'} icon={HiDocumentText} as='div'>
               Posts
+            </Sidebar.Item>
+          </Link>
+          }
+                    {
+            currentUser.isAdmin && <Link to='/dashboard?tab=users'>
+            <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
+              Users
             </Sidebar.Item>
           </Link>
           }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,31 +1,34 @@
-import { useEffect, useState } from 'react';
-import {useLocation} from 'react-router-dom'
-import DashSidebar from '../components/DashSidebar';
-import DashProfile from '../components/DashProfile';
-import DashPosts from '../components/DashPosts';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
 
 // this page have differents tabs (for admin)
 //the content in right is dynamic depending on the tab
 export default function Dashboard() {
   const location = useLocation();
-  const [tab, setTab] = useState('')
+  const [tab, setTab] = useState("");
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search)
-    const tabFromUrl = urlParams.get('tab')
-    if (tabFromUrl) setTab(tabFromUrl)
-  }, [location.search])
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) setTab(tabFromUrl);
+  }, [location.search]);
 
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className="min-h-screen flex flex-col md:flex-row">
       {/* sidebar */}
-      <div className='md:w-56'>
+      <div className="md:w-56">
         <DashSidebar />
       </div>
       {/* right side */}
-      {tab === 'profile' && <DashProfile/>}
+      {tab === "profile" && <DashProfile />}
       {/* posts */}
-      {tab === 'posts' && <DashPosts/>}
+      {tab === "posts" && <DashPosts />}
+      {/* users */}
+      {(tab === "users") && <DashUsers />}
     </div>
-  )
+  );
 }
