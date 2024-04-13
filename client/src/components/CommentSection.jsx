a50b57113bc4c7ef0bd4b52c386a92c3f1777173
woody <gelius24@hotmail.com>
2024-04-13 16:04:05 -0400

    like functionality

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Alert, Button, TextInput, Textarea } from "flowbite-react";
 import { useEffect, useState } from "react";
 import Comment from "./Comment";
@@ -9,6 +9,7 @@ export default function CommentSection({ postId }) {
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
+  const navigate = useNavigate();
   
   useEffect(() => {
     const getComments = async () => {
@@ -48,6 +49,26 @@ export default function CommentSection({ postId }) {
       setCommentError(error.message);
     }
   };
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) return navigate('/sign-in')
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, 
+      {method: 'PUT'})
+      if (res.ok) {
+        const data = await res.json();
+        setComments(comments.map((comment) => 
+          comment._id === commentId ? {
+            ...comment,
+            likes: data.likes,
+            numberOfLikes: data.numberOfLikes
+          } : comment
+        ))
+      }
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
@@ -112,7 +133,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map(comment => {
-            return <Comment key={comment._id} comment={comment}/>
+            return <Comment key={comment._id} comment={comment} onLike={handleLike}/>
           })}
         </>
       )}
